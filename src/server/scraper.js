b8b3366e1ab4a6dd0542a38887518d7c5dc07241
David Bisrat <dabisrat@gmail.com>
2016-12-25 23:42:51 -0500

    feat(server): add a web scraper to get frame data

---
+++ src/server/scraper.js
@@ -0,0 +1,113 @@
+var request = require('request');
+var cheerio = require ('cheerio');
+var fs = require('fs');
+var path = require('path');
+
+// _____________________ using bluebird Promises with promisify ________________________________________________
+var bluebird = require('bluebird')
+var promisifyWriteFile = bluebird.promisify(fs.writeFile);
+var promisifyRequest = bluebird.promisify(request);
+
+module.exports = function () {
+  var characters = [
+    'Alex', 'Birdie', 'Cammy', 'ChunLi', 'Dhalsim', 'Fang', 'Guile',
+    'Karin', 'Ken', 'Laura', 'MBison', 'Nash', 'Necalli', 'RMika',
+    'Rashid','Ryu','Vega','Zangief'
+  ];
+  characters.forEach(function (character) {
+    var charURL = 'http://wiki.shoryuken.com/Street_Fighter_V/' + character;
+    var dirPath = path.resolve('src/server/frameData/' + character + 'FrameData.html');
+    promisifyRequest(charURL)
+      .then(function (content) {
+        promisifyWriteFile(dirPath, content.body);
+      })
+      .catch(function (err){
+        if (err) {
+          throw new Error (err);
+        }
+      });
+      console.log( character + ' frameData saved');
+  });
+}
+// ___________________________________________________________________________________________________________
+
+// ________________________________ using native promises ____________________________________________________
+// module.exports = function () {
+//   var characters = [
+//     'Alex', 'Birdie', 'Cammy', 'ChunLi', 'Dhalsim', 'Fang', 'Guile',
+//     'Karin', 'Ken', 'Laura', 'MBison', 'Nash', 'Necalli', 'RMika',
+//     'Rashid','Ryu','Vega','Zangief'
+//   ];
+//   characters.forEach(function (character) {
+//     var charURL = 'http://wiki.shoryuken.com/Street_Fighter_V/' + character;
+//     var dirPath = path.resolve('src/server/frameData/' + character + 'FrameData.html');
+//     requestData(charURL, dirPath)
+//       .then(writeData)
+//       .catch(handleErr)
+//     console.log( character + ' frameSata saved');
+//   });
+
+//   function requestData(url, writePath) {
+//     return new Promise(function (resolve, reject) {
+//       request(url, function (err, response, html){
+//         if (err) {
+//          return reject(err);
+//         }
+//         resolve({
+//           html: html, 
+//           writePath: writePath
+//         });
+//       });
+//     });
+//   };
+
+//   function writeData(content) {
+//     return new Promise(function (resolve, reject) {
+//       fs.writeFile(content.writePath, content.html, function(err) {
+//         if (err) {
+//           return reject(err);
+//         }
+//         resolve(content);
+//       });
+//     });
+//   };
+
+//   function handleErr(err){
+//     if (err) {
+//       throw new Error(err)
+//     }
+//   };
+// }
+//___________________________________________________________________________________________________________
+
+// _____________________________ using raw callbacks _________________________________________________________
+
+// module.exports = function() {
+//   var char = [
+//     'Alex', 'Birdie', 'Cammy', 'ChunLi', 'Dhalsim', 'Fang', 'Guile',
+//     'Karin', 'Ken', 'Laura', 'MBison', 'Nash', 'Necalli', 'RMika',
+//     'Rashid','Ryu','Vega','Zangief'
+//   ];
+//   var length = char.length, i = 0;
+//   for (i; i < length; i++) {
+//     var uri = 'http://wiki.shoryuken.com/Street_Fighter_V/' + char[i];
+//     var dir = path.resolve('src/server/frameData/' + char[i] + 'FrameData.html');
+//     httpRequest(uri, i, dir, char)
+//   }
+
+//   function httpRequest(uri, i, dir, char) {
+//     request(uri, function (err, response, html) {
+//       if (!err) {
+//         fs.writeFile(dir, html, function (err) {
+//           if (!err) {
+//             console.log(char[i],'data saved');
+//           } else {
+//             throw new Error (err);
+//           }
+//         });
+//       }
+//     });
+//   }
+// };
+//___________________________________________________________________________________________________________
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var scraper = require('./scraper')
+
+scraper();
 
 const app = express();
-console.log(path.resolve())
 app.use(express.static(path.resolve('src/client')));
-app.use(bodyParser.json())
+ app.use(bodyParser.json())
 
-app.get('/', (req, res) =>  {
-  res.status(200).sendFile('index')
-})
 
 app.listen(3000, () =>{
   console.log('connected to server listening on port 3000')
